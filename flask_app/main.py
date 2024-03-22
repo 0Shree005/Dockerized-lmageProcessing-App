1f53a1722d070ec9e53cc1b5459f3d452c51104b
0Shree005 <shreepingale12@gmail.com>
2024-03-22 20:45:45 +0530

    hardcoded image detection test

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,15 +1,28 @@
+import numpy as np
 import cv2
 
-img = cv2.imread('assets/cars_on_road.jpeg', 1)
+img = cv2.imread("assets/Cars.jpeg", 0)
+img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+template = cv2.imread("assets/singleCar.jpg", 0)
+template = cv2.resize(template, (0, 0), fx=0.5, fy=0.5)
 
-resizeImg = cv2.resize(img, (0, 0), fx= 0.5, fy= 0.5)
+height, width = template.shape
 
-rotateImg = cv2.rotate(resizeImg, cv2.ROTATE_180)
+methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
 
+for method in methods:
+    img2 = img.copy()
 
-# cv2.imwrite('assets/modified_image.jpg', rotateImg)
+    result = cv2.matchTemplate(img2, template, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        location = min_loc
+    else:
+        location = max_loc
+    
+    boottom_right = (location[0] + width, location[1] + height)
+    cv2.rectangle(img2, location, boottom_right, 255, 5)
 
-cv2.imshow('Image', rotateImg)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("Match", img2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
